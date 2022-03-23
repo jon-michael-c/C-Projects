336ed414b1f93e77e055e924a5d7eeb5d7a33454
jon-michael-c <jonmichael.cubano@gmail.com>
2022-03-23 09:32:31 -0400

    added sqrt

--- 02-Calculator/calculator.cpp
+++ 02-Calculator/calculator.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 enum Oper {
-    Add=0, Sub, Mult, Div, Exp
+    Add=0, Sub, Mult, Div, Sqrt
 };
 
 struct Calculator {
@@ -26,8 +26,8 @@ struct Calculator {
                 return num1 * num2;
             case Div:
                 return num1 / num2;
-            case Exp:
-                return pow(num1, num2);
+            case Sqrt:
+                return pow(num1, 0.5);
         }
 
     }
@@ -35,31 +35,35 @@ struct Calculator {
 
 int main() {
     
-    double num1, num2;
+    bool done = false;
+    double num1=0, num2=0;
     int mode;   
 
-
-    cout << "Enter two numbers" << endl;
-    cout << "Number 1: " << endl;
-    cin >> num1;
-    cout << "Number 2: " << endl;
-    cin >> num2;
-    cout << "Enter Operation" << endl;
-    cout << "0 - ADD" << endl;
-    cout << "1 - SUBTRACT" << endl;
-    cout << "2 - MULTIPLY" << endl;
-    cout << "3 - DIVIDE" << endl;
-    cout << "4 - EXPONENT" << endl;
-    cin >> mode;
-
-    Calculator calc(num1, num2);
-
-    cout << calc.calculate(mode) << endl;
-
-    
+    while(!done) {
+        cout << "Enter Operation" << endl;
+        cout << "0 - ADD" << endl;
+        cout << "1 - SUBTRACT" << endl;
+        cout << "2 - MULTIPLY" << endl;
+        cout << "3 - DIVIDE" << endl;
+        cout << "4 - SQUARE ROOT" << endl;
+        cin >> mode;
 
 
+        cout << "Enter two numbers" << endl;
+        cout << "Number 1: " << endl;
+        cin >> num1;
+        if(mode != 4) {
+            cout << "Number 2: " << endl;
+            cin >> num2;
+        }
+        
+        Calculator calc(num1, num2);
 
+        cout << calc.calculate(mode) << endl;
+
+        
+        
+    }    
     return 0;
 
 }
