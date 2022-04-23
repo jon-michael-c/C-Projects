6c4facc58c40f4534e74f83eae821ccc7c9159cd
jon-michael-c <jonmichaelcubano@gmail.com>
2022-04-23 17:18:08 -0400

    testing

--- 03-Student-Grades/src/main.cpp
+++ 03-Student-Grades/src/main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <fstream>
 #include <string>
+#include <regex>
 
 using namespace std;
 
@@ -16,13 +17,27 @@ int main() {
 
     vector<entry> entries = records.getEntries();
     
-    vector<rate> rates = records.getPassPerInstructor();
-    vector<rate> rates2 = records.getPassPerCourse();
+    vector<rate> rates = records.getPassPerCourse();
+    vector<rate> rates2 = records.getPassRateSpring();
 
     vector<student> students = records.getStudents();
     vector<instructor> instructors = records.getInstructors();
     vector<course> courses = records.getCourses();
 
+    /*
+    records.addEntry("E0003", 1115, "I12", "T04", "S10", "A+");
+    records.addEntry("E018", 3115, "I12", "T69", "S97", "A+");
+    records.addEntry("E018", 3130, "I12", "T69", "S97", "A+");
+    cout << entries[2917].emplid << endl;
+    cout << entries[2917].courseno << endl;
+    */
+
+
+    updateCSV(records.getEntries());
+
+    
+
+    /*
     for(auto i : students) {
         cout << i.id << endl;
         cout << i.course << endl;
@@ -38,6 +53,12 @@ int main() {
         cout << i.sectionid << endl;
     }
 
+    */
+    
+    if(!1) {
+
+    cout << regex_match("W", regex("(W)(.*)")) << endl;
+    }
     for(auto i : rates) {
         cout << i.id << " ";
         cout << i.num << " ";
@@ -46,14 +67,13 @@ int main() {
     }
 
     for(auto i : rates2) {
-        cout << i.id << " ";
-        cout << i.num << " ";
-        cout << i.den << " ";
-        cout << i.rate << endl;
+    cout << i.id << endl;
+    cout << i.num << endl;
+    cout << i.den << endl;
+    cout << i.rate << endl;
+    
     }
-
 }
-
 
 
 
