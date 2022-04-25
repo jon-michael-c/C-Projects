edaab959500be61f7b6fde556179379a26df1427
jon-michael-c <jonmichaelcubano@gmail.com>
2022-04-24 21:38:58 -0400

    removed printing functions

--- 03-Student-Grades/src/utilFuncs.h
+++ 03-Student-Grades/src/utilFuncs.h
@@ -1,75 +1,76 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <iomanip> // put_time
+#include <vector>
+#include <regex>
 
 using namespace std;
 
 #include "structs.h"
-#include "recordUtil.h"
 
 #ifndef UTIL_HEADER
 #define UTIL_HEADER
 
 
-
-void printPPI(recordUtil records) {
-    cout << "Processing Data...";
-    vector<rate> ratePPI = records.getPassPerInstructor();
-    cout << "Done";
-    cout << "------------ Pass Rate Per Instructor -----------------------" << endl;
-    for(auto i : ratePPI) {
-        cout << " Instructor: " << i.id;
-        cout << " Passed: " << i.num;
-        cout << " Total: " << i.den;
-        cout << " Pass Rate: " << i.rate << "%" << endl;
-    }
-    cout << "---------------------------------------------------------" << endl;
-
+void printHeader(string head) {
+    cout << "______________ " << head << " ______________" << endl;
+}
+void printLineBorder() {
+    cout << "______________" << endl;
 }
 
-void printPPC(recordUtil records) {
-    cout << "Processing Data...";
-    vector<rate> rate = records.getPassPerCourse();
-    cout << "Done";
-    cout << "------------ Pass Rate Per Course -----------------------" << endl;
-    for(auto i : rate) {
-        cout << " Course: " << i.id;
-        cout << " Passed: " << i.num;
-        cout << " Total: " << i.den;
-        cout << " Pass Rate: " << i.rate << "%" << endl;
-    }
-    cout << "---------------------------------------------------------" << endl;
+bool isFall(string termid) {
+            vector<string> fallTerms = {"T04", "T08", "T12", "T16", "T20", "T23"};
+            for(auto i : fallTerms) {
+                if(termid == i) {
+                    return true;
+                }
+            }
 
+            return false;
 }
 
-void printWRI(recordUtil records) {
-    cout << "Processing Data...";
-    vector<rate> rate = records.getWRatePerInstructor();
-    cout << "Done";
-    cout << "------------ Withdraw Rate Per Instructor -----------------------" << endl;
-    for(auto i : rate) {
-        cout << " Instructor: " << i.id;
-        cout << " Passed: " << i.num;
-        cout << " Total: " << i.den;
-        cout << " Pass Rate: " << i.rate << "%" << endl;
+bool isSpring(string termid) {
+    vector<string> springTerms = {"T02", "T06", "T10", "T14", "T18", "T21"};
+    for(auto i : springTerms) {
+        if(termid == i) {
+            return true;
+        }
     }
-    cout << "---------------------------------------------------------" << endl;
 
+    return false;
+}       
+
+bool isFailed(string grade) {
+    if(regex_match(grade, regex("(F)(.*)"))) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-void printWRC(recordUtil records) {
-    cout << "Processing Data...";
-    vector<rate> rate = records.getWRatePerCourse();
-    cout << "Done";
-    cout << "------------ Withdraw Rate Per Course -----------------------" << endl;
-    for(auto i : rate) {
-        cout << " Course: " << i.id;
-        cout << " Passed: " << i.num;
-        cout << " Total: " << i.den;
-        cout << " Pass Rate: " << i.rate << "%" << endl;
-    }
-    cout << "---------------------------------------------------------" << endl;
 
+bool isWithrW(string grade) {
+    if(regex_match(grade, regex("(W)(.*)"))) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+string getTime() {
+    auto now = std::chrono::system_clock::now();
+    auto UTC = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
+
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+    std::stringstream datetime;
+    datetime << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+
+    return datetime.str();
+    
 }
 
 #endif
+
 
